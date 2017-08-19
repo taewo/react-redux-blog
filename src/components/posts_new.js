6864a8c441ad7da0d8d3a54a1a58dac235c88ce3
taewoong <moondingie5@gmail.com>
2017-08-19 22:00:22 +0900

    Refactoring

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
   render() {
     return (
       <div>
+        <Link to="/">
+          go back
+        </Link>
         PostNew!
       </div>
     )
